b8b64acd6f0aa454970c8cf1efb02ffd121cd47f
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-10-28 16:43:58 -0300

    Implementação da tela de confirmação para a imersão

---
+++ src/app/ConfirmacaoImersao/page.tsx
@@ -0,0 +1,11 @@
+import Confirmacao from '@/components/imersao/ConfirmacaoImersao/Confirmacao'
+
+
+export default function Imersionista() {
+  return (
+    <main className="min-h-screen bg-dark-purple text-white">
+      <Confirmacao/>
+      
+    </main>
+  )
+}
---
+++ src/components/imersao/ConfirmacaoImersao/Confirmacao.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Image from 'next/image'
+
+export default function Confirmacao() {
+  return (
+    <div className='pb-12'>
+      <div className='pt-8'>
+        <Image
+            src="/img/LOGO-ROXA-LETREIRO-HORIZONTAL1.png"
+            alt="Logo"
+            width={320}
+            height={147}
+            className="m-auto mt-8 "
+        />  
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-center  ">
+        <h1 className='text-accent font-bold text-5xl mt-14 md:mt-8 '>PARABÉNS!</h1>
+        <Image
+            src="/img/developer_male.png"
+            alt="Logo"
+            width={320}
+            height={147}
+            className="md:mt-8 "
+        />  
+      </div>
+      <p className='text-center  md:text-xl'>Você está cadastrado na imersão da Fábirca de <br /> Software, acompanhe seu email para ficar por dentro <br /> das notícias.</p>  
+      
+    </div>
+  )
+}
