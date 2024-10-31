10ac212e553e348f0da469ab491e6aad9bac2395
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-10-31 18:02:28 -0300

    ✨ feat: Adicionando validação nos campos de entrada

--- src/components/Inputs/InputText.tsx
+++ src/components/Inputs/InputText.tsx
@@ -1,13 +1,18 @@
+import { FieldError } from 'react-hook-form';
 interface InputTextProps {
-  label: string
-  placeholder: string
-  type: string
+  label: string;
+  placeholder: string;
+  type: string;
+  register: any;
+  error?: FieldError;
 }
 
 export default function InputText({
   label,
   placeholder,
   type,
+  register,
+  error,
 }: InputTextProps) {
   return (
     <div>
@@ -16,7 +21,9 @@ export default function InputText({
         type={type}
         placeholder={placeholder}
         className="w-80 md:w-72 lg:w-96 h-9 rounded-md text-black p-2"
+        {...register}
       />
+        {error && <p className="text-red-500">{error.message}</p>} 
     </div>
   )
 }
