0f803e8f1a3015c7ef61a3b8ab359262c3a8dedb
João <joaovictorqueiroz39@gmail.com>
2024-11-07 14:26:50 -0300

    formulario de imersão respondendo a função do usuário

--- src/components/imersao/Imersao.tsx
+++ src/components/imersao/Imersao.tsx
@@ -1,29 +1,30 @@
-'use client'
-import { FiChevronDown } from 'react-icons/fi'
-import Radio from './RadioGroup/RadioGroup'
-import Image from 'next/image'
-import InputSelect from '../Inputs/InputSelect'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { imersaoFormSchemaType, imersaoFormSchema } from './imersaoFormSchema'
+"use client";
+import { FiChevronDown } from "react-icons/fi";
+import Radio from "./RadioGroup/RadioGroup";
+import Image from "next/image";
+import InputSelect from "../Inputs/InputSelect";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { imersaoFormSchemaType, imersaoFormSchema } from "./imersaoFormSchema";
 
 export default function Imersao() {
   const {
     register,
     handleSubmit,
+    watch,
     setValue,
     formState: { errors },
   } = useForm<imersaoFormSchemaType>({
-    mode: 'all',
+    mode: "all",
     resolver: zodResolver(imersaoFormSchema),
-  })
+  });
 
-  console.log(errors)
+  console.log(errors);
 
   const handleForm = (data: imersaoFormSchemaType) => {
-    console.log(data)
-  }
-
+    console.log(data);
+  };
+  const funcao = watch("funcao");
   return (
     <div className="pb-12">
       <div className="pt-8">
@@ -40,7 +41,7 @@ export default function Imersao() {
           <div className="flex flex-col gap-4 items-center justify-center md:grid grid-cols-2 md:gap-8 mt-12">
             <InputSelect
               label="Função"
-              register={register('funcao')}
+              register={register("funcao")}
               error={errors.funcao}
             >
               <option value="" hidden>
@@ -53,7 +54,7 @@ export default function Imersao() {
             </InputSelect>
             <InputSelect
               label="Período"
-              register={register('periodo')}
+              register={register("periodo")}
               error={errors.periodo}
             >
               <option value="" hidden>
@@ -81,32 +82,77 @@ export default function Imersao() {
           <FiChevronDown className="ml-2 h-5 w-5 transform scale-125" />
         </div>
         <div>
-          <Radio
-            label={'Next.js & React'}
-            setValue={setValue}
-            name={'nextJsReact'}
-            error={errors.nextJsReact}
-            errorText="A habilidade é obrigatória"
-          ></Radio>
-          <Radio
-            label={'Python & Django'}
-            setValue={setValue}
-            name={'pythonDjango'}
-            error={errors.pythonDjango}
-            errorText="A habilidade é obrigatória"
-          ></Radio>
-          <Radio
-            label={'Java & Spring'}
-            setValue={setValue}
-            name={'javaSpring'}
-            error={errors.javaSpring}
-            errorText="A habilidade é obrigatória"
-          ></Radio>
+          {funcao === "Front-End" && (
+            <>
+              <Radio
+                label={"Next.js & React"}
+                setValue={setValue}
+                name={"nextJsReact"}
+                error={errors.nextJsReact}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+              <Radio
+                label={"Vite & React"}
+                setValue={setValue}
+                name={"nextJsReact"}
+                error={errors.nextJsReact}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+              <Radio
+                label={"TypeScript"}
+                setValue={setValue}
+                name={"nextJsReact"}
+                error={errors.nextJsReact}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+              <Radio
+                label={"JavaScript"}
+                setValue={setValue}
+                name={"nextJsReact"}
+                error={errors.nextJsReact}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+            </>
+          )}
+          {funcao === "Back-End" && (
+            <>
+              <Radio
+                label={"Python & Django"}
+                setValue={setValue}
+                name={"pythonDjango"}
+                error={errors.pythonDjango}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+              <Radio
+                label={"PHP & Laravel"}
+                setValue={setValue}
+                name={"pythonDjango"}
+                error={errors.pythonDjango}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+              <Radio
+                label={"Node.JS & Express"}
+                setValue={setValue}
+                name={"pythonDjango"}
+                error={errors.pythonDjango}
+                errorText="A habilidade é obrigatória"
+              ></Radio>
+            </>
+          )}
+          {funcao === "Dados" && (
+            <Radio
+              label={"Java & Spring"}
+              setValue={setValue}
+              name={"javaSpring"}
+              error={errors.javaSpring}
+              errorText="A habilidade é obrigatória"
+            ></Radio>
+          )}
         </div>
         <button className="text-secondary font-bold text-xl bg-light-grey flex items-center justify-center m-auto p-4 rounded-md mt-8">
           INSCREVER-SE
         </button>
       </form>
     </div>
-  )
+  );
 }
