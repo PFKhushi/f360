3ea0676c69193d39a42dd8bc0bf1865c6c0427c0
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2024-11-08 12:52:04 -0300

    :arrow_up: correção de Admin.py + atualizando requirements.txt + :recycle: limpeza de migrações

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -1,5 +1,43 @@
 from django.contrib import admin
 from .models import Usuarios, Experiencias
 
-admin.site.register(Usuarios)
-admin.site.register(Experiencias)
+@admin.register(Usuarios)
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome', 
+        'username', 
+        'email_institucional', 
+        'rgm', 
+        'curso', 
+        'cargo', 
+        'setor', 
+        'situacao', 
+        'is_bolsista', 
+        'is_estagiario', 
+        'data_criacao'
+    )
+    list_filter = (
+        'curso', 
+        'cargo', 
+        'setor', 
+        'situacao', 
+        'is_bolsista', 
+        'is_estagiario'
+    )
+    search_fields = ('nome', 'username', 'rgm', 'email_institucional')
+    ordering = ('nome', 'data_criacao')
+    readonly_fields = ('data_criacao', 'data_atualizacao')
+
+@admin.register(Experiencias)
+class ExperienciasAdmin(admin.ModelAdmin):
+    list_display = (
+        'usuario', 
+        'tecnologias', 
+        'senioridade', 
+        'descricao', 
+        'data_criacao'
+    )
+    list_filter = ('tecnologias', 'senioridade')
+    search_fields = ('usuario__nome', 'tecnologias', 'descricao')
+    ordering = ('usuario', 'data_criacao')
+    readonly_fields = ('data_criacao', 'data_atualizacao')
