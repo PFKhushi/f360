9a3f0ce994e24bb50e87c093c87c8d0eb380bebc
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-11-27 15:45:51 -0300

    ♻️ refactor: Ajustes nos campos da tela de perfil do usuário

--- src/components/meu-perfil/MyProfile.tsx
+++ src/components/meu-perfil/MyProfile.tsx
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import InputText from '../Inputs/InputText'
+import InputSelect from '../Inputs/InputSelect'
 import { AtualizarPerfilFormSchema } from '@/components/meu-perfil/AtualizarPerfilFormSchema'
 
 interface AtualizarPerfil {
@@ -68,7 +69,7 @@ export default function MyProfile({ user }: AtualizarPerfil) {
   return (
     <form
       onSubmit={handleSubmit(handleForm)}
-      className="w-full md:p-4 flex flex-col justify-center items-center bg-dark-purple rounded-xl text-white"
+      className="-ml-2 md:-ml-0 w-80 md:w-full md:p-4 flex flex-col justify-center items-center bg-dark-purple rounded-xl text-white"
     >
       <h1 className="md:text-3xl mb-8 mt-8">Editar Perfil</h1>
       <div className="flex flex-col md:grid grid-cols-2 justify-center items-center gap-4 md:gap-12">
@@ -88,46 +89,94 @@ export default function MyProfile({ user }: AtualizarPerfil) {
           error={errors.email_institucional}
           defaultValue={user?.email_institucional}
         />
-        <InputText
-          label="CURSO"
-          placeholder="Insira seu curso"
-          type="text"
+        <InputSelect
+          label="Curso"
           register={register('curso')}
           error={errors.curso}
-          defaultValue={user?.curso}
-        />
-        <InputText
+          valueDefault={user?.curso}
+        >
+          <option value="" hidden>
+            Selecione um curso
+          </option>
+          <option value="Análise e Desenvolvimento de Sistemas">
+            Análise e Desenvolvimento de Sistemas
+          </option>
+          <option value="Ciência da Computação">
+            Ciência da Computação
+          </option>
+          <option value="Sistemas para Internet">
+            Sistemas para Internet
+          </option>
+          <option value="Ciência de Dados">Ciência de Dados</option>
+          <option value="Outros">Outros</option>
+        </InputSelect>
+        <InputSelect
           label="CARGO"
-          placeholder="Insira seu cargo"
-          type="text"
           register={register('cargo')}
           error={errors.cargo}
-          defaultValue={user?.curso}
-        />
-        <InputText
+          valueDefault={user?.cargo}
+        >
+          <option value="" hidden>
+            Selecione um cargo
+          </option>
+          <option value="NENHUM">Nenhum</option>
+          <option value="GESTOR">Gestor</option>
+          <option value="IMERSIONISTA">Imersionista</option>
+          <option value="NOVATO">Novato</option>
+          <option value="TECH LEADER">Tech Leader</option>
+          <option value="VETERANO">Veterano</option>
+        </InputSelect>
+        <InputSelect
           label="SETOR"
-          placeholder="Insira seu setor"
-          type="text"
           register={register('setor')}
           error={errors.setor}
-          defaultValue={user?.setor}
-        />
-         <InputText
-          label="PERIODO"
-          placeholder="Insira seu periodo"
-          type="text"
-          register={register('periodo')}
-          error={errors.periodo}
-          defaultValue={user?.periodo}
-        />
-         <InputText
+          valueDefault={user?.setor}
+        >
+          <option value="" hidden>
+            Selecione uma função
+          </option>
+          <option value="BACK">Back-End</option>
+          <option value="FRONT">Front-End</option>
+          <option value="PO">PO</option>
+          <option value="DADOS">Dados</option>
+          <option value="QA">Quality Assurance</option>
+          <option value="UIUX">UI/UX</option>
+          <option value="DEVOPS">DevOps</option>
+          <option value="MOBILE">Mobile</option>
+          <option value="IA">Inteligência Artificial</option>
+          <option value="JOGOS">Jogos</option>
+        </InputSelect>
+        
+        <InputSelect
+            label="Período"
+            register={register('periodo')}
+            error={errors.periodo}
+            valueDefault={errors?.periodo?.message}
+          >
+            <option value="" hidden>
+              Selecione um período
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+        </InputSelect>
+        <InputText
           label="TELEFONE"
           placeholder="Insira seu telefone"
           type="text"
           register={register('telefone')}
           error={errors.telefone}
           defaultValue={user?.telefone}
-        />
+        /> 
       </div>
       <button
         type="submit"
