3cf168f7e6d681851ceac1aec17ebb115ff751f1
thiagomvn <114946613+thiagomvn@users.noreply.github.com>
2025-04-16 16:21:41 -0300

    rotas de teste do participante criadas

--- projeto/api_rest/urls.py
+++ projeto/api_rest/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from . import views
+from api_rest import views
+from rest_framework.routers import DefaultRouter
+from api_rest.views import ParticipanteList
+
+router = DefaultRouter()
+router.register('participantes', ParticipanteList, basename='participantes')
+urlpatterns = router.urls
+
+
 
 
 
@@ -12,19 +20,20 @@ from . import views
 # ]
 
 from django.urls import path
-from . import views
+from api_rest import views
 
-urlpatterns = [
-    # Rotas de Autenticação
-    path('user/login/', views.LoginUsuario.as_view(), name='login_usuario'),
-    path('user/logout/', views.LogoutUsuario.as_view(), name='logout_usuario'),#Invalida sessões JWT
+# urlpatterns = [
+#     # Rotas de Autenticação
+#     path('user/login/', views.LoginUsuario.as_view(), name='login_usuario'),
+#     path('user/logout/', views.LogoutUsuario.as_view(), name='logout_usuario'),#Invalida sessões JWT
 
-    # CRUD de Usuários
-    path('user/register/', views.CriarUsuario.as_view(), name='registrar_usuario'),
-    path('user/list/', views.UsuarioList.as_view(), name='lista_usuario'),
-    path('user/<int:id>/edit/', views.EditarUsuario.as_view(), name='editar_usuario'),  
+#     # CRUD de Usuários
+#     path('user/register/', views.CriarUsuario.as_view(), name='registrar_usuario'),
+#     path('user/list/', views.UsuarioList.as_view(), name='lista_usuario'),
+#     path('user/<int:id>/edit/', views.EditarUsuario.as_view(), name='editar_usuario'),  
 
-    # Perfil do Usuário
-    path('user/me/', views.UsuarioAtual.as_view(), name='usuario_atual'), #Substitui a necessidade de passar ID/Username
-    path('user/password/', views.AlterarSenha.as_view(), name='alterar_senha'),#Fluxo seguro de troca de senha
-]
+#     # Perfil do Usuário
+#     path('user/me/', views.UsuarioAtual.as_view(), name='usuario_atual'), #Substitui a necessidade de passar ID/Username
+#     path('user/password/', views.AlterarSenha.as_view(), name='alterar_senha'),#Fluxo seguro de troca de senha
+# ]
+
