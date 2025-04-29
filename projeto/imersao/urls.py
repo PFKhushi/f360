d6045e2dbbfeca540ab87426ca094ed3ca2a351d
Pedro Firmino <mafiazuka@gmail.com>
2025-04-29 10:17:28 -0300

    ✨ feat: Gestão da imersão completa, faltando apenas atualizar as rotas e definir melhor as regras de uso

--- projeto/imersao/urls.py
+++ projeto/imersao/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import ImersaoViewSet, TecnologiaViewSet, AreaFabricaViewSet, PalestraViewSet, FormularioInscricaoViewSet
+from .views import ImersaoViewSet, TecnologiaViewSet, AreaFabricaViewSet, PalestraViewSet, FormularioInscricaoViewSet, InteresseAreaViewSet, PresencaPalestraViewSet, ParticipacaoImersaoViewSet
 
 router = DefaultRouter()
 
@@ -8,6 +8,9 @@ router.register('tecnologia', TecnologiaViewSet, basename='tecnologia')
 router.register('area_fabrica', AreaFabricaViewSet, basename='area_fabrica')
 router.register('palestra', PalestraViewSet, basename='palestra')
 router.register('formulario_inscricao', FormularioInscricaoViewSet, basename='formulario_inscricao')
+router.register('interesse_area', InteresseAreaViewSet, basename='interesse_area') # Retirar depois
+router.register('presenca_palestra', PresencaPalestraViewSet, basename='presenca_palestra')
+router.register('participacao_imersao', ParticipacaoImersaoViewSet, basename='participacao_imersao')
 
 
 
