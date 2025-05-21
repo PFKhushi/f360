9e1b323a2f1f715a6f602fb279e749b2def77a98
Leoncio Fernandes <leonciofernandes@gmail.com>
2025-05-21 11:48:45 -0300

    :sparkles: feat: Implementada animação no Banner + Ajustes

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
+import LogoAnimationBanner from './LogoAnimationBanner'
 
 export default function Banner() {
   return (
-    <section className='flex w-full h-120 md:h-160'>
+    <section className="relative flex w-full h-120 md:h-160 bg-cover bg-center bg-[url('/images/banner.png')] pt-24">
+    
+      <div className='absolute w-1/2 h-full top-0 bg-linear-to-r from-primary-5 to-transparent to-10%'/>
+      <div className='absolute w-1/2 h-full top-0 right-0 bg-linear-to-l from-primary-5 to-transparent to-10%'/>
+      <div className='absolute w-full h-full top-0 right-0 bg-linear-to-t from-primary-4 to-transparent to-5%'/>
 
-      <div className='hidden lg:flex items-center justify-center bg-gradient-to-r from-primary-5 from-35% via-primary-4 via-75% to-primary-3 w-1/2'>
-        <div className='flex flex-col gap-10 text-white max-w-85'>
-          <p className='text-4xl font-coolvetica'>Onde capacitam-se futuros profissionais de trabalho na Unipê</p>
-          <p className='text-xl font-louis-george-cafe'>O futuro do trabalho é agora.</p>
+      <div className='flex w-full max-w-360 h-full mx-auto z-1'>
+
+        <div className='hidden lg:flex items-center justify-center w-1/2 h-full'>
+          <div className='flex flex-col gap-10 text-white max-w-85'>
+            <p className='text-4xl font-coolvetica'>Onde capacitam-se futuros profissionais de trabalho na Unipê</p>
+            <p className='text-xl font-louis-george-cafe'>O futuro do trabalho é agora.</p>
+          </div>
         </div>
-      </div>
 
-      <div className='relative lg:w-1/2'>
-        <div
-          className='hidden lg:block absolute w-10 h-full bg-gradient-to-r from-primary-3 to-transparent'
-        />
-        <picture>
-          <img
-            src="/images/logos/banner-fs-semfundo.png"
-            alt="Banner Fábrica de Software"
-            className='w-full h-full object-cover'
-          />
-        </picture>
+        <div className='flex justify-center items-center w-full lg:w-1/2 h-full'>
+          <div className='w-76 md:w-110 h-full'>
+            <LogoAnimationBanner/>
+          </div>
+        </div>
+
       </div>
 
     </section>
